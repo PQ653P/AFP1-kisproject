4facf8b9a7333310a4fae1b336d6ad6495b51d6c
Dániel Árvai <arvai96@gmail.com>
2020-10-07 17:57:01 +0200

    Játék funkció implementálása

--- game.js
+++ game.js
@@ -1,17 +1,19 @@
 window.onload = function() {
-    var player_score = 0;
-    var cpu_score = 0;
+	var score_player = 0;
+	var score_cpu = 0;
 
     var btn_rock = document.getElementById('btn-rock');
     var btn_paper = document.getElementById('btn-paper');
     var btn_scissors = document.getElementById('btn-scissors');
 
     var score_diplay_player = document.getElementById('score-player');
-    var score_display_cpu = document.getElementById('score-cpu');
+	var score_display_cpu = document.getElementById('score-cpu');
+	var game_result_display = document.getElementById('game-result');
+	var game_details_display = document.getElementById("game-details");
 
     score_diplay_player.innerHTML = 0;
     score_display_cpu.innerHTML = 0;
-	
+
 	btn_rock.addEventListener('click', play_rock);
 	btn_paper.addEventListener('click', play_paper);
 	btn_scissors.addEventListener('click', play_scissors);
@@ -26,5 +28,26 @@ window.onload = function() {
 
 	function play_scissors() {
 		play("Olló");
+	}
+
+	function play(choice) {
+		cpu_choice = Math.floor(Math.random() * (3 - 1) + 1);
+		cpu_choice_str = cpu_choice === 0 ? "Kő" : cpu_choice === 1 ? "Papír" : "Olló";
+		if (choice === cpu_choice_str) {
+			game_result_display.innerHTML = "Döntetlen";
+		} else if (
+			(choice === "Kő" && cpu_choice_str === "Olló") ||
+			(choice === "Papír" && cpu_choice_str === "Kő") ||
+			(choice === "Olló" && cpu_choice_str === "Papír")
+		) {
+			score_player++;
+			game_result_display.innerHTML = "A játékos nyert!";
+		} else {
+			score_cpu++;
+			game_result_display.innerHTML = "A gép nyert!";
+		}
+		score_diplay_player.innerHTML = score_player;
+		score_display_cpu.innerHTML = score_cpu;
+		game_details_display.innerHTML = choice + " vs. " + cpu_choice_str;
 	}
 }
