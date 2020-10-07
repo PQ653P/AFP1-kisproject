e152b27aa334f2bdcf9265b3d58d84cb7acbf0a5
Dániel Árvai <arvai96@gmail.com>
2020-10-07 14:26:00 +0200

    Azonosítók hozzárendelése a HTML tagekhez

---
+++ game.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    var player_score = 0;
+    var cpu_score = 0;
+
+    var btn_rock = document.getElementById('btn-rock');
+    var btn_paper = document.getElementById('btn-paper');
+    var btn_scissors = document.getElementById('btn-scissors');
+
+    var score_diplay_player = document.getElementById('score-player');
+    var score_display_cpu = document.getElementById('score-cpu');
+}
